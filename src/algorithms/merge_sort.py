cca6d3ce3cae44a7b9c1e812122c374d992a359b
Denis Capeto <deniscapeto@gmail.com>
2022-10-30 21:12:21 +0100

    Update flake8 version to 5.0.4 and fix affected files

--- src/algorithms/merge_sort.py
+++ src/algorithms/merge_sort.py
@@ -16,7 +16,6 @@ def merge_sort(list):
 
     result_list = []
 
-
     for i in range(len(list)):
         if len(left_sorted) == 0:
             result_list.append(right_sorted.pop(0))
@@ -24,24 +23,25 @@ def merge_sort(list):
         if len(right_sorted) == 0:
             result_list.append(left_sorted.pop(0))
             continue
-        
+
         if right_sorted[0] < left_sorted[0]:
             result_list.append(right_sorted.pop(0))
         else:
             result_list.append(left_sorted.pop(0))
-    
+
     print(f'result list: {result_list}')
 
     return result_list
 
-input1 = [83,20,9,50,115,61,17]
+
+input1 = [83, 20, 9, 50, 115, 61, 17]
 result1 = merge_sort(input1)
 print(f'Merge sort: {input1} -> {result1}')
 assert result1 == [9, 17, 20, 50, 61, 83, 115]
 
 print('')
 
-input2 = [54,26,93,17,77,31,44,55,20]
+input2 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 result2 = merge_sort(input2)
 print(f'Merge sort: {input2} -> {result2}')
 assert result2 == [17, 20, 26, 31, 44, 54, 55, 77, 93]
